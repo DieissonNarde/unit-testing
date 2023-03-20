625ebc5956fc60661b6ee068bba29523056161be
DieissonNarde <dieissonnarde@gmail.com>
2023-03-20 17:47:38 -0400

    Aula 5: realtime results with watch parameter

--- src/calculaValor.js
+++ src/calculaValor.js
@@ -6,11 +6,20 @@ function calcularMontante (capital, taxa, periodo) {
 
 function arredondar (valor) {
   const precisao = 100
-  const arredondado = Math.round((valor + Number.EPSILON) * precisao) / precisao
+  const arredondado =
+    Math.round((valor + Number.EPSILON) * precisao) / precisao
   return arredondado
+}
+
+function calcularPrestacoes (montante, numeroParcelas) {
+  const prestacaoBase = arredondar(montante / numeroParcelas)
+  const resultado = Array(numeroParcelas).fill(prestacaoBase)
+
+  return resultado
 }
 
 module.exports = {
   calcularMontante,
-  arredondar
+  arredondar,
+  calcularPrestacoes
 }
