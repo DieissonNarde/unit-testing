99ce9e81e8ea1ed4c6c95f1ca3515566e1a78437
DieissonNarde <dieissonnarde@gmail.com>
2023-03-22 22:43:26 -0400

    Aula 16: tests with supertest

--- 03-consulta-credito/src/server.js
+++ 03-consulta-credito/src/server.js
@@ -1,5 +1,11 @@
 const app = require('./app')
 
+const db = require('./db')
+
+db.sequelize.sync().then(async () => {
+  await console.log('Conectado ao banco de dados!')
+})
+
 app.listen(5678, () => {
-  console.log('Aplicação ouvindo na porta 5678!')
+  console.log('Bootcamp desenvolvedor back end - Tópicos especiais, Aplicação ouvindo na porta 5678!')
 })
